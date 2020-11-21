dc50259d96458a6ba8297d6bc8eada31395f2039
Michelle Ranly <meranly8@gmail.com>
2020-11-21 18:40:12 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,9 +7,8 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    
-    self.characters.collect do |character|
-      character.name
-    end
+    characters.collect do |character|
+      "#{character.name} - #{character.show.name}"
+        end
   end
 end
