941f43a4b421aad24a527111c17106b2f2417b5b
Michelle Ranly <meranly8@gmail.com>
2020-11-21 17:19:33 +0000

    Automatically backed up by Learn

--- db/migrate/20201121171812_create_characters.rb
+++ db/migrate/20201121171812_create_characters.rb
@@ -1,4 +1,9 @@
 class CreateCharacters < ActiveRecord::Migration[5.2]
   def change
+    create_table :characters do |t|
+      t.string :name
+      i.integer :actor_id
+      i.integer :character_id
+    end
   end
 end
