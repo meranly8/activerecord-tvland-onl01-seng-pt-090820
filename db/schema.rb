4b094a3046b0219e9910becef21b4fd657e92126
Michelle Ranly <meranly8@gmail.com>
2020-11-21 17:39:02 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_21_173018) do
+ActiveRecord::Schema.define(version: 2020_11_21_173805) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_11_21_173018) do
 
   create_table "shows", force: :cascade do |t|
     t.string "name"
+    t.integer "network_id"
   end
 
 end
