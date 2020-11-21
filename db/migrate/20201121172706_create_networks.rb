f81dc33ac78d1980c9690c4e6cd8b9ac38c341c9
Michelle Ranly <meranly8@gmail.com>
2020-11-21 17:29:17 +0000

    Automatically backed up by Learn

--- db/migrate/20201121172706_create_networks.rb
+++ db/migrate/20201121172706_create_networks.rb
@@ -1,6 +1,6 @@
 class CreateNetworks < ActiveRecord::Migration[5.2]
   def change
-    create_table do |t|
+    create_table :networks do |t|
       t.string :call_letters
       t.string :channel
     end
